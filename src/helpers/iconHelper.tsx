c1d141959e1cce0c0350aa105f7e364b55c2a616
Madi <148999737+Agadyr@users.noreply.github.com>
2025-06-21 20:17:11 +0500

    Update iconHelper.tsx

--- src/helpers/iconHelper.tsx
+++ src/helpers/iconHelper.tsx
@@ -1,9 +1,9 @@
 const iconHelper = (icon: string) => {
     const iconLower = icon.toLowerCase()
     if (iconLower.includes('sun') || iconLower.includes('clear') || iconLower.includes('mostly sunny') || iconLower.includes('ясно') || iconLower.includes('солн')) {
-        return '/icons/Image8.svg'
+        return '/icons/image8.svg'
     } else if (iconLower.includes('cloudy') || iconLower.includes('облачно')) {
-            return '/icons/Image7.svg'
+            return '/icons/image7.svg'
     } else if (iconLower.includes('rain') || iconLower.includes('дождь')) {
         return '/icons/Raincloud.svg'
     } else if (iconLower.includes('storm') || iconLower.includes('drizzle') || iconLower.includes('thunder') || iconLower.includes('гроза') || iconLower.includes('гром')) {
@@ -13,7 +13,7 @@ const iconHelper = (icon: string) => {
     } else if (iconLower.includes('wind')) {
         return '/icons/wind.svg'
     }
-    return '/icons/Image7.svg'
+    return '/icons/image7.svg'
 }
 
 export default iconHelper
