58d428b90f165f8a4252f17f483143d8c8bb2669
Agadyr <kairambekovmadi@gmail.com>
2025-06-21 20:12:23 +0500

    Удалены неиспользуемые SVG-иконки и обновлены пути к оставшимся иконкам в компонентах и вспомогательных функциях.

--- src/helpers/iconHelper.tsx
+++ src/helpers/iconHelper.tsx
@@ -1,11 +1,11 @@
 const iconHelper = (icon: string) => {
     const iconLower = icon.toLowerCase()
     if (iconLower.includes('sun') || iconLower.includes('clear') || iconLower.includes('mostly sunny') || iconLower.includes('ясно') || iconLower.includes('солн')) {
-        return '/icons/Image 8.svg'
+        return '/icons/Image8.svg'
     } else if (iconLower.includes('cloudy') || iconLower.includes('облачно')) {
-            return '/icons/Image 7.svg'
+            return '/icons/Image7.svg'
     } else if (iconLower.includes('rain') || iconLower.includes('дождь')) {
-        return '/icons/Rain cloud.svg'
+        return '/icons/Raincloud.svg'
     } else if (iconLower.includes('storm') || iconLower.includes('drizzle') || iconLower.includes('thunder') || iconLower.includes('гроза') || iconLower.includes('гром')) {
         return '/icons/storm.svg'
     } else if (iconLower.includes('fog')) {
@@ -13,7 +13,7 @@ const iconHelper = (icon: string) => {
     } else if (iconLower.includes('wind')) {
         return '/icons/wind.svg'
     }
-    return '/icons/Image 7.svg'
+    return '/icons/Image7.svg'
 }
 
 export default iconHelper
